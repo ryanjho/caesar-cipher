9aba56f1ba2f9863e8939e5ab20a15cc7ee452ac
Ryan Ho <ryanjho90@gmail.com>
2020-07-31 11:05:07 +0800

    Updates

---
+++ precache-manifest.528a486562adcf926fd7e2ecede820d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b989c245b6930c255ce01e5ce73d100",
+    "url": "/caesar-cipher-react/index.html"
+  },
+  {
+    "revision": "f5e31f6b14e0ddca6b9c",
+    "url": "/caesar-cipher-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dd9905eb82c4cf23333",
+    "url": "/caesar-cipher-react/static/js/2.e91efb7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/caesar-cipher-react/static/js/2.e91efb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e31f6b14e0ddca6b9c",
+    "url": "/caesar-cipher-react/static/js/main.4765b532.chunk.js"
+  },
+  {
+    "revision": "525ef8f968fa77323edd",
+    "url": "/caesar-cipher-react/static/js/runtime-main.78971be0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/caesar-cipher-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b911bd3c595e607805a0aa6f35a07aa5.js"
+  "/caesar-cipher-react/precache-manifest.528a486562adcf926fd7e2ecede820d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/caesar-cipher-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

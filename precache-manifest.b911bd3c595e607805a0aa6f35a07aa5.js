2f250123f8ed29d684b987f42f851f96e4396178
Ryan Ho <ryanjho90@gmail.com>
2020-07-31 11:02:09 +0800

    Updates

---
+++ precache-manifest.b911bd3c595e607805a0aa6f35a07aa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd446c8483c6a858bc6be15f811048a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e193509d3473df914e4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dd9905eb82c4cf23333",
+    "url": "/static/js/2.e91efb7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e91efb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e193509d3473df914e4",
+    "url": "/static/js/main.bc453515.chunk.js"
+  },
+  {
+    "revision": "c707374690d6e3d32809",
+    "url": "/static/js/runtime-main.ac40ac26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
